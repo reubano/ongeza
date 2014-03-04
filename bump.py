d9028b87a86d7ba61a45e743b4da54831507d860
Reuben Cummings <reubano@gmail.com>
2014-03-04 21:40:34 +0300

    Refactor to be more object oriented

--- bump.py
+++ bump.py
@@ -6,7 +6,7 @@
 import os
 from sys import exit
 from argparse import RawTextHelpFormatter, ArgumentParser
-from subprocess import call, check_output
+from script import Project, Git, fail
 
 parser = ArgumentParser(
 	description="description: bump makes following the Semantic Versioning"
@@ -19,9 +19,9 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument(
 	'-t', '--type', dest='bump_type', action='store', choices=['m', 'n', 'p'],
 	help="version bump type:\n"
-		"  m = major - x.0.0\n"
-		"  n = minor - 1.y.0\n"
-		"  p = patch - 1.0.z")
+		"  m = major - [x].0.0\n"
+		"  n = minor - x.[y].0\n"
+		"  p = patch - x.y.[z]")
 
 group.add_argument(
 	'-s', '--set', dest='version', action='store',
@@ -32,8 +32,25 @@ parser.add_argument(
 	help='increase output verbosity')
 
 parser.add_argument(
+	'-c', '--skip-commit', action='store_true', help='skip commiting version '
+	' bumped files')
+
+parser.add_argument(
 	'-g', '--skip-tag', action='store_true', help='skip tagging git repo with '
 	'the bumped version number')
+
+parser.add_argument(
+	'-f', '--tag-format', action='store',
+	default='Version {version} Release',
+	help='git tag messgae format')
+
+parser.add_argument(
+	'-F', '--commit-format', action='store',
+	default='Bump to version {version}',
+	help='git commit message format')
+
+group.add_argument(
+	'-p', '--push', action='store_true', help='push to the remote origin')
 
 parser.add_argument(
 	dest='dir', nargs='?', default=os.curdir, type=str,
@@ -41,108 +58,40 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-def fail(message, *args):
-	print >> stderr(), "error:", message.format(*args)
-	exit(1)
-
-def has_tag(git_dir):
-	# Check if repo has any git tags.
-	if os.path.isdir(git_dir):
-		return check_output('cd %s; git tag' % (git_dir), shell=True)
-	else:
-		raise Exception('%s is not a directory' % (git_dir))
-
-def get_version(git_dir):
-	# Get the current release version from git.
-	if os.path.isdir(git_dir):
-		cmd = 'cd %s; git tag | grep v | tail -n1' % (git_dir)
-		version = check_output(cmd, shell=True)
-		version = version.lstrip('v').rstrip()
-		return version.split('-')[0]
-	else:
-		raise Exception('%s is not a directory' % (git_dir))
-
-def set_version(old_version, new_version, file, dir, pattern=None):
-	if not old_version:
-		# find lines in file containing pattern
-		cmd = 'cd %s; grep -ine "%s" %s' % (dir, pattern, file)
-		lines = check_output(cmd, shell=True)
-
-		# find first line containing a version number
-		cmd = 'echo "%s" | grep -im1 "[0-9]*\.[0-9]*\.[0-9]*"' % (lines)
-		rep_line = check_output(cmd, shell=True)
-		repl_line_num = rep_line.split(':')[0]
-
-		# replace with new version number
-		cmd = ("cd %s; sed -i '' '%ss/[0-9]*\.[0-9]*\.[0-9]*/%s/g' %s"
-			% (dir, repl_line_num, new_version, file))
-	else:
-		cmd = ("cd %s; sed -i '' 's/%s/%s/g' %s"
-			% (dir, old_version, new_version, file))
-
-	return call(cmd, shell=True)
-
-def get_dev_version(version):
-	return map(int, version.split('.'))
-
-def git_add(files, git_dir):
-	files = ' '.join(files)
-	return call('cd %s; git add %s' % (git_dir, files), shell=True)
-
-def git_commit(message, git_dir):
-	return call("cd %s; git commit -m '%s'" % (git_dir, message), shell=True)
-
-def git_tag(version, git_dir):
-	cmd = ("cd %(g)s; git tag -sm 'Version %(v)s Release' v%(v)s"
-		% {'g': git_dir, 'v': version})
-
-	return call(cmd, shell=True)
-
-def bump_version(bump_type, curr_version):
-	switch = {
-		'm': lambda: [curr_version[0] + 1, 0, 0],
-		'n': lambda: [curr_version[0], curr_version[1] + 1, 0],
-		'p': lambda: [curr_version[0], curr_version[1], curr_version[2] + 1]}
-
-	return '.'.join(map(str, switch.get(bump_type)()))
 
 def main():
+	project = Project()
+	git = Git(args.dir)
+
 	files = os.listdir(args.dir)
 	file_name = ('pearfarm.spec', 'setup.cfg', 'setup.py', )
 	file_ext = ('.xml', '.json')
 	versioned_files = filter(lambda x: x.endswith(file_ext), files)
 	[versioned_files.append(f) for f in files if f in file_name]
-	is_tagged = has_tag(args.dir)
 
-	if is_tagged:
-		curr_version = get_version(args.dir)
-		dev_version = get_dev_version(curr_version)
-
-	if (not is_tagged and args.bump_type):
+	if (not project.has_tag and args.bump_type):
 		fail("No git tags found, please run with the '-s' option")
-	elif (is_tagged and not args.bump_type and not args.version):
-		string = 'Current version: %s' % curr_version
-	elif (is_tagged and args.bump_type):
-		new_version = bump_version(args.bump_type, dev_version)
-		[set_version(curr_version, new_version, file, args.dir)
-			for file in versioned_files]
+	elif (project.has_tag and not args.bump_type and not args.version):
+		string = 'Current version: %s' % project.version
+	elif (project.has_tag and args.bump_type):
+		new_version = project.bump_version(args.bump_type)
+		[project.set_version(new_version, file) for file in versioned_files]
 
-		string = 'Bump from version %s to %s' % (curr_version, new_version)
-	else: # set the version
+		string = 'Bump from version %s to %s' % (project.version, new_version)
+	else:  # set the version
 		# TODO: check args.version validity
 		new_version = args.version
-		[set_version(None, new_version, file, args.dir)
-			for file in versioned_files]
-
+		[project.set_version(new_version, file) for file in versioned_files]
 		string = 'Set to version %s' % new_version
 
-	if (args.version or (args.bump_type and is_tagged)):
-		message = 'Bump to version %s' % new_version
-		git_add(versioned_files, args.dir)
-		git_commit(message, args.dir)
+	if (args.version or (args.bump_type and project.has_tag)):
+		message = args.commit_format.format(version=new_version)
+		git.add(versioned_files)
+		git.commit(message)
 
-	if (not args.skip_tag and version):
-		git_tag(version, args.dir)
+	if (not args.skip_tag and project.version):
+		message = args.tag_format.format(version=project.version)
+		git.tag(message, project.version)
 	elif not args.skip_tag:
 		string = "No version found to tag"
 
---
+++ script/__init__.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+lego
+----
+
+compares ebay and amazon lego set prices
+
+"""
+
+__title__ = 'lego'
+__package_name__ = 'lego'
+__author__ = 'Reuben Cummings'
+__description__ = 'compares ebay and amazon lego set prices'
+__email__ = 'reubano@gmail.com'
+__version__ = '0.8.1'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Reuben Cummings'
+
+import os
+from subprocess import call, check_output
+
+
+def fail(message):
+	print message
+	exit(1)
+
+
+def sh(cmd):
+	call(cmd, shell=True)
+
+
+class Project(object):
+	"""
+	class representing a project object.
+	"""
+
+	def __init__(self, dir, **kwargs):
+		"""
+		Parameters
+		----------
+		dir : str
+			the project directory
+		"""
+		self.current_tag = None
+		self.dir = dir
+
+	@property
+	def has_tag(self):
+		# Check if repo has any git tags.
+		if os.path.isdir(self.dir):
+			return check_output('cd %s; git tag' % (self.dir), shell=True)
+		else:
+			raise Exception('%s is not a directory' % (self.dir))
+
+	@property
+	def version(self):
+		# Get the current release version from git.
+		if os.path.isdir(self.dir):
+			cmd = 'cd %s; git tag | grep v | tail -n1' % (self.dir)
+			version = check_output(cmd, shell=True)
+			version = version.lstrip('v').rstrip()
+			return version.split('-')[0]
+		else:
+			raise Exception('%s is not a directory' % (self.dir))
+
+	@property
+	def dev_version(self):
+		return map(int, self.version.split('.'))
+
+	def set_version(self, new_version, file, dir, pattern=None):
+# 		def inject_version(match):
+# 			before, old, after = match.groups()
+# 			changed.append(True)
+# 			return before + version_number + after
+#
+# 		with open(filename, "r") as f:
+# 			data_str = re.sub(
+# 				r"^(\s*%s\s*=\s*')(.+?)(')(?sm)" % pattern, inject_version, f.read())
+# 		if len(changed) < 1:
+# 			fail(
+# 				"could not set init file version. pattern {} not found in {}",
+# 				pattern, filename)
+# 		write(filename, data_str)
+
+		if not self.version:
+			# find lines in file containing pattern
+			cmd = 'cd %s; grep -ine "%s" %s' % (dir, pattern, file)
+			lines = check_output(cmd, shell=True)
+
+			# find first line containing a version number
+			cmd = 'echo "%s" | grep -im1 "[0-9]*\.[0-9]*\.[0-9]*"' % (lines)
+			rep_line = check_output(cmd, shell=True)
+			repl_line_num = rep_line.split(':')[0]
+
+			# replace with new version number
+			cmd = ("cd %s; sed -i '' '%ss/[0-9]*\.[0-9]*\.[0-9]*/%s/g' %s"
+				% (dir, repl_line_num, new_version, file))
+		else:
+			cmd = ("cd %s; sed -i '' 's/%s/%s/g' %s"
+				% (dir, self.version, new_version, file))
+
+		return call(cmd, shell=True)
+
+	def bump_version(self, bump_type):
+		switch = {
+			'm': lambda: [self.version[0] + 1, 0, 0],
+			'n': lambda: [self.version[0], self.version[1] + 1, 0],
+			'p': lambda: [self.version[0], self.version[1], self.version[2] + 1]}
+
+		return '.'.join(map(str, switch.get(bump_type)()))
+
+	@property
+	def codename(self):
+		if self.last_release:
+			return self.last_release.codename
+
+	@property
+	def last_release(self):
+		if self.release_count > 0:
+			return self.releases[-1]
+
+	@property
+	def release_count(self):
+		return len(self.releases)
+
+	@property
+	def last_tag(self, tags):
+		return tags[-1]
+
+	@property
+	def info(self, current_tag, last_tag, version_num):
+		"""
+		prints project version information and exits without error.
+		"""
+		INFO_FMT = "project version information:\nlatest tag:   {}\n"
+		INFO_FMT += "current tag:  {}\nversion id:   {}"
+		INFO_FMT.format(last_tag, current_tag, version_num)
+
+
+class Release(object):
+	"""
+	class representing a version release.
+	"""
+
+	def __init__(self, project, version_num, datestr=None, summaries=None):
+		"""
+		Parameters
+		----------
+		version_num: string version in the format: [x].[x].[x]
+		"""
+		self.project = project
+		self.codename = project.codename
+
+	def _bump_num(self, version_num, bump_type):
+		"""
+		Parameters
+		----------
+		version_num: string version name.
+		bump_type: version bump type. one of:
+			m = major - [x].0.0
+			n = minor - x.[y].0
+			p = patch - x.y.[z]
+		Returns
+		-------
+		concatenated string of the incremented version name.
+		"""
+		# split the version number into a list of ints..
+		try:
+			version = [int(v) for v in version_num.split(".")]
+			switch = {
+				"major": lambda: [version[0] + 1, 0, 0],
+				"minor": lambda: [version[0], version[1] + 1, 0],
+				"patch": lambda: [version[0], version[1], version[2] + 1]}
+			return ".".join(map(str, switch.get(bump_type)()))
+		except ValueError:
+			fail("version string: %i is an invalid format.." % version_num)
+
+	def bump(self, bump_type):
+		"""
+		Parameters
+		----------
+		bump_type: version bump type. one of:
+			m = major - [x].0.0
+			n = minor - x.[y].0
+			p = patch - x.y.[z]
+		"""
+		self.version_num = self._bump_num(self.version_num, bump_type)
+
+
+class Git(object):
+	"""
+	class representing Git commands.
+	"""
+	def __init__(self, dir):
+		"""
+		Parameters
+		----------
+		dir: directory containing the git project
+		"""
+		self.dir = dir
+
+	@property
+	def current_tag(self):
+		"""
+		Returns
+		-------
+		string of the current git tag on the git index, not the latest
+			tag version created.
+		"""
+		return sh("git describe").strip()
+
+	@property
+	def tags(self):
+		"""
+		Returns
+		-------
+		list of git tags, sorted by the date of the commit it points to.
+		"""
+		return sh("git for-each-ref --format='%(tag)' refs/tags").splitlines()
+
+	@property
+	def is_clean(self):
+		"""
+		Returns
+		-------
+		boolean if there is a dirty index.
+		"""
+		return str(sh("git diff --quiet")) == "0"
+
+	def add(self, files):
+		files = ' '.join(files)
+		return call('cd %s; git add %s' % (self.dir, files), shell=True)
+
+	def commit(self, message):
+		return call("cd %s; git commit -m '%s'" % (self.dir, message), shell=True)
+
+	def tag(self, message, version):
+		cmd = "cd %s; git tag -sm '%s' v%s" % (self.dir, message, version)
+		return call(cmd, shell=True)
+
+	def diff_files(self):
+		"""
+		Returns
+		-------
+		list of string names of the dirty files.
+		"""
+		files = sh("git diff --minimal --numstat")
+		return [x.split("\t")[-1] for x in files.splitlines()]
+
+	def stash(self):
+		"""
+		stashes current changes in git.
+		"""
+		sh("git stash")
+		return True
+
+	def push(self):
+		"""
+		pushes current branch and tags to remote.
+		"""
+		# don't call --all here on purpose..
+		sh("git push && git push --tags")
+
+	def ensure_clean_index(self):
+		"""
+		ensures the current git staging index has no uncommitted or stashed changes.
+
+		"""
+		if self.is_clean():
+			return True
+		files = self.diff_files()
+		msg = """
+		cannot bump the version with a dirty git index.
+		fix uncommitted files by stashing, committing,
+		or resetting the following files:
+
+		{}
+		""".format("\n	".join(files))
+		fail(msg)
