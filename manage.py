66e404d8b3680910da5157bb17bae4a83df07c4f
Reuben Cummings <reubano@gmail.com>
2014-03-17 14:07:45 +0300

    Refactor manager and add install command

--- manage.py
+++ manage.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from manager import Manager
-from subprocess import call
+from subprocess import call, check_call
 
 manager = Manager()
+
 
 @manager.command
 def clean():
@@ -15,21 +16,29 @@ def clean():
 	cmd = '%s;%s;%s' % (cmd1, cmd2, cmd3)
 	return call(cmd, shell=True)
 
+
 @manager.command
 def lint():
-	"""check style with flake8"""
-	cmd1 = 'flake8 --max-complexity=9 --ignore=W191,E126,E128,E203 bump.py'
-	cmd2 = 'flake8 --max-complexity=9 --ignore=W191,E126,E128,E203 script'
-	cmd = '%s;%s' % (cmd1, cmd2)
-	return call(cmd, shell=True)
+	"""Check style with flake8"""
+	call('flake8', shell=True)
+
 
 @manager.command
 def test():
 	"""Run tests"""
-	cmd1 = 'nosetests'
-	cmd2 = 'python tests/testscript.py'
-	cmd = '%s;%s' % (cmd1, cmd2)
-	return call(cmd, shell=True)
+	try:
+		check_call('nosetests', shell=True) is 0
+	except Exception as err:
+		raise SystemExit(err)
+	else:
+		call('python tests/testscript.py', shell=True)
+
+
+@manager.command
+def install():
+	"""Build egg"""
+	check_call('python setup.py install', shell=True)
+
 
 if __name__ == '__main__':
 	manager.main()
