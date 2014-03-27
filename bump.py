9ec782e9242c035c09054112ecc3b49ac225a2fa
Reuben Cummings <reubano@gmail.com>
2014-03-27 09:36:22 +0300

    Fix git tagging when setting initial version

--- bump.py
+++ bump.py
@@ -67,7 +67,7 @@ def main():
 
 		if (not project.version and args.bump_type):
 			raise Exception(
-				"No git tags found, please run with the '-s' option")
+				"No git tags found, please run with '-s and -T' options")
 		elif (project.version and not args.bump_type and not args.version):
 			msg = 'Current version: %s' % project.version
 		elif project.is_dirty:
@@ -84,7 +84,7 @@ def main():
 					project.version, new_version)
 			else:
 				raise Exception("Couldn't find a version to bump.")
-		else:  # set the version
+		elif args.version:  # set the version
 			new_version = args.version
 			if not project.check_version(new_version):
 				raise Exception(
@@ -99,9 +99,13 @@ def main():
 			git.add(project.versioned_files)
 			git.commit(message)
 
-		if (project.bumped and args.tag and project.version):
-			message = args.tag_format.format(version=project.version)
-			git.tag(message, project.version)
+		tag = (project.bumped and args.tag and project.version)
+		tag = (tag or (args.version and args.tag and not project.version))
+
+		if tag:
+			version = (project.version or args.version)
+			message = args.tag_format.format(version=version)
+			git.tag(message, version)
 		elif args.tag:
 			raise Exception("Couldn't find a version to bump. Nothing to tag.")
 
