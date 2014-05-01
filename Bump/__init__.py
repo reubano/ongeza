d726af03dc6740f241900b0c40fd1c7697864e85
Reuben Cummings <reubano@gmail.com>
2014-05-01 13:25:21 +0300

    Bump to version 1.1.0

--- Bump/__init__.py
+++ Bump/__init__.py
@@ -15,7 +15,7 @@ __author__ = 'Reuben Cummings'
 __description__ = 'An automated way to follow the Semantic Versioning '
 __description__ += 'Specification'
 __email__ = 'reubano@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Reuben Cummings'
 
