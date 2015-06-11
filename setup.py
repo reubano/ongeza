092dc8f50d8cff06a59d61b78b9e7f287ba42bd6
gregory nicholas <nicholas.g.gregory@gmail.com>
2015-06-11 11:17:51 -0700

    - updates setup.py, moves keybump to bin/ dir

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ keybump
 
 manage your versioning like a boss .
 
-  - keybump is an opinionated command-line app to manage versioning workflow + dist + releasing.
-  - keybump makes following the semantic versioning specification (http://semver.org) a breeze.
-  - keybump helps to automate the tedious task of summarizing changes from one version to the next by intelligently parsing the commit messages.
+  - keybump is an opinionated command-line app to manage versioning workflow + dist + releasing
+  - keybump makes following the semantic versioning specification (http://semver.org) a breeze
+  - keybump helps to automate the tedious task of summarizing changes from one version to the next by intelligently parsing the commit messages
 
 
 links:
 ``````
 
-* `docs: <http://gregorynicholas.github.io/keybump>`_
+* `homepage: <http://gregorynicholas.github.io/keybump>`_
 * `source: <http://github.com/gregorynicholas/keybump>`_
-* `issues: <http://github.com/gregorynicholas/keybump/issues>`_
-* `package: <http://packages.python.org/keybump>`_
+* `python-package: <http://packages.python.org/keybump>`_
 * `travis-ci: <http://travis-ci.org/gregorynicholas/keybump>`_
 * `development version: <http://github.com/gregorynicholas/keybump/zipball/master#egg=keybump-dev>`_
+* `github-issues: <http://github.com/gregorynicholas/keybump/issues>`_
 * `semantic versioning specification: <http://semver.org>`_
 
 """
@@ -37,6 +37,9 @@ with open('keybump/__init__.py', 'r') as f:
   v = re.findall(r'__version__\s*=\s*\'(.*)\'', f.read())
   __version__ = v[0]
 
+with open("requirements.txt", "r") as f:
+  requires = f.readlines()
+
 
 setup(
   name="keybump",
@@ -48,30 +51,31 @@ setup(
   description="manage your versioning like a boss .",
   long_description=__doc__,
 
-  install_requires=[
-    'pyyaml==3.10',
-    'shell==1.0.1',
-  ],
+  install_requires=requires,
 
   scripts=[
     'bin/keybump',
-  ],
-
-  py_modules=[
   ],
 
   packages=[
     'keybump',
   ],
 
-  # test_suite="tests",
-  test_suite='nose.collector',
-  tests_require=[
-    'nose==1.2.1',
-    'nose-cov',
-    'mock==1.0.1',
+  namespace_packages=[
   ],
 
+  py_modules=[
+  ],
+
+  test_suite='nose.collector',
+  tests_require=[
+    'nose',
+    'nose-cov',
+    'mock',
+  ],
+
+  dependency_links=[
+  ],
   license='MIT',
   zip_safe=False,
   platforms='any',
