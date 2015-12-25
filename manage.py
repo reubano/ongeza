2b7f3bcd3f6acf21349a2e0c105b709dabe2acdb
Reuben Cummings <reubano@gmail.com>
2015-12-25 19:26:44 +0300

    Refactor manager and script tests

--- manage.py
+++ manage.py
@@ -1,44 +1,145 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+
+""" A script to manage development tasks """
+
+from __future__ import (
+    absolute_import, division, print_function, with_statement,
+    unicode_literals)
+
+from os import path as p
+from subprocess import call, check_call, CalledProcessError
 
 from manager import Manager
-from subprocess import call, check_call
 
 manager = Manager()
+BASEDIR = p.dirname(__file__)
+
+
+def upload_():
+    """Upload distribution files"""
+    check_call('twine upload %s' % p.join(BASEDIR, 'dist', '*'), shell=True)
+
+
+def sdist_():
+    """Create a source distribution package"""
+    check_call(p.join(BASEDIR, 'helpers', 'srcdist'))
+
+
+def wheel_():
+    """Create a wheel package"""
+    check_call(p.join(BASEDIR, 'helpers', 'wheel'))
 
 
 @manager.command
 def clean():
-	"""remove Python file artifacts"""
-	cmd1 = "find . -name '*.pyc' -exec rm -f {} +"
-	cmd2 = "find . -name '*.pyo' -exec rm -f {} +"
-	cmd3 = "find . -name '*~' -exec rm -f {} +"
-	cmd = '%s;%s;%s' % (cmd1, cmd2, cmd3)
-	call(cmd, shell=True)
+    """Remove Python file and build artifacts"""
+    exit(call(p.join(BASEDIR, 'helpers', 'clean')))
 
 
 @manager.command
-def lint():
-	"""Check style with flake8"""
-	call('flake8', shell=True)
+def check():
+    """Check staged changes for lint errors"""
+    exit(call(p.join(BASEDIR, 'helpers', 'check-stage')))
+
+
+@manager.arg('where', 'w', help='Modules to check')
+@manager.arg('strict', 's', help='Check with pylint')
+@manager.command
+def lint(where=None, strict=False):
+    """Check style with linters"""
+    args = 'pylint --rcfile=tests/standard.rc -rn -fparseable bump'
+
+    try:
+        check_call(['flake8', where] if where else 'flake8')
+        check_call(args.split(' ')) if strict else None
+    except CalledProcessError as e:
+        exit(e.returncode)
 
 
 @manager.command
-def test():
-	"""Run tests"""
-	try:
-		check_call('nosetests', shell=True) is 0
-	except Exception as err:
-		raise SystemExit(err)
-	else:
-		call('python tests/testscript.py', shell=True)
+def pipme():
+    """Install requirements.txt"""
+    exit(call('pip install -r requirements.txt'.split(' ')))
 
 
 @manager.command
-def install():
-	"""Build egg"""
-	check_call('python setup.py install', shell=True)
+def require():
+    """Create requirements.txt"""
+    cmd = 'pip freeze -l | grep -vxFf dev-requirements.txt > requirements.txt'
+    exit(call(cmd, shell=True))
+
+
+@manager.arg('where', 'w', help='test path', default=None)
+@manager.arg(
+    'stop', 'x', help='Stop after first error', type=bool, default=False)
+@manager.command
+def test(where=None, stop=False, tox=False):
+    """Run nose, tox, and script tests"""
+    opts = '-xv' if stop else '-v'
+    opts += 'w %s' % where if where else ''
+
+    try:
+        check_call(('nosetests %s' % opts).split(' '))
+        check_call(['python', p.join(BASEDIR, 'tests', 'test.py')])
+    except CalledProcessError as e:
+        exit(e.returncode)
+
+
+@manager.command
+def register():
+    """Register package with PyPI"""
+    exit(call('python %s register' % p.join(BASEDIR, 'setup.py'), shell=True))
+
+
+@manager.command
+def release():
+    """Package and upload a release"""
+    try:
+        sdist_()
+        wheel_()
+        upload_()
+    except CalledProcessError as e:
+        exit(e.returncode)
+
+
+@manager.command
+def build():
+    """Create a source distribution and wheel package"""
+    try:
+        sdist_()
+        wheel_()
+    except CalledProcessError as e:
+        exit(e.returncode)
+
+
+@manager.command
+def upload():
+    """Upload distribution files"""
+    try:
+        upload_()
+    except CalledProcessError as e:
+        exit(e.returncode)
+
+
+@manager.command
+def sdist():
+    """Create a source distribution package"""
+    try:
+        sdist_()
+    except CalledProcessError as e:
+        exit(e.returncode)
+
+
+@manager.command
+def wheel():
+    """Create a wheel package"""
+    try:
+        wheel_()
+    except CalledProcessError as e:
+        exit(e.returncode)
 
 
 if __name__ == '__main__':
-	manager.main()
+    manager.main()
